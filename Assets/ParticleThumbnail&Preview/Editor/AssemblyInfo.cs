c6166949dddc952c55b9ce4697101fbe0b3dffd4
Fardin Haque <fardinhaque.work@gmail.com>
2026-05-09 03:45:55 +0600

    cleanups

--- Assets/ParticleThumbnail&Preview/Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-// Exposes editor internals to the test assembly so preview and thumbnail behavior can be validated safely through unit tests.
-
-[assembly: InternalsVisibleTo("ParticleThumbnailAndPreview.Editor.Tests")]
